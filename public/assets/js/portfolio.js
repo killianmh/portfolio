de3456c281e7806e2f9d45d3f724d9082f0ddaa1
killianmh <killianmh@gmail.com>
2018-06-18 13:14:59 -0700

    slick

--- public/assets/js/portfolio.js
+++ public/assets/js/portfolio.js
@@ -1,20 +1,17 @@
 // Wait to attach our handlers until the DOM is fully loaded.
 $(function(){
+
+    $('.img-slider').slick({
+        arrows: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
     
     $(".toggle-modal").on("click", function(){
         // alert("you clicked the button");
         var dataBtn = $(this).attr("data-btn");
-        $("#project-modal-" + dataBtn).modal(
-            
-        );
-
-        $('.img-slider').slick({
-            arrows: true,
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1
-        });
-
+        $("#project-modal-" + dataBtn).modal();
     });
 
     
