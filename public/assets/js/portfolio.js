44cc29e565f33e81bf2d6a632901e2ef957e6b73
killianmh <killianmh@gmail.com>
2018-06-28 14:14:29 -0700

    project title text formatting 2

--- public/assets/js/portfolio.js
+++ public/assets/js/portfolio.js
@@ -1,12 +1,5 @@
 // Wait to attach our handlers until the DOM is fully loaded.
 $(function(){
-
-    // $('.img-slider').slick({
-    //     arrows: true,
-    //     infinite: true,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1
-    // });
     
     $(".toggle-modal").on("click", function(){
         // alert("you clicked the button");
