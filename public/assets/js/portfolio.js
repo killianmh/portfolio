9ba46082e2c83cd4dd2d4d7637391af638aa8294
killianmh <killianmh@gmail.com>
2018-07-03 13:43:08 -0700

    smoothscroll to bottom of landing-page

--- public/assets/js/portfolio.js
+++ public/assets/js/portfolio.js
@@ -12,7 +12,7 @@ $(function(){
         if(this.hash !== ""){
             event.preventDefault();
             let hash = this.hash;
-            $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function() {
+            $('html, body').animate({scrollTop: $(hash).offset().bottom}, 800, function() {
                 window.location.hash = hash;
             })
         }
